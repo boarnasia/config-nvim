b55e8144fe71200a26aeaffb64872c39be1411da
Uehara Masato <masato.uehara.1975@gmail.com>
2026-02-15 03:32:22 +0900

    feat: add zenhan.nvim
    - add copilot-chat config

---
+++ lua/plugins/copilot-cat.lua
@@ -0,0 +1,8 @@
+return {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        auto_insert_mode = false,
+        language = "Japanese",
+    },
+}
