26da81e3f8720bc6623dfdf5a0c0afd300c0acf4
Masato Uehara <masato.uehara.1975@gmail.com>
2026-02-16 05:34:08 +0900

    update editor.lua

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,7 +1,9 @@
 return {
     {
         "boarnasia/zenhan.vim",
-        cond = vim.fn.has("wsl") == 1,
+        cond = function()
+            return vim.fn.has("win32") == 1 or vim.fn.has("wsl") == 1
+        end,
         opts = {},
     },
 }
