d3fd7cbbabd5ea00b04007f4426443309a69a5e6
Uehara Masato <masato.uehara.1975@gmail.com>
2026-02-19 21:08:27 +0900

    remove a file being forgot to delete

--- lua/plugins/copilot-cat.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cond = vim.g.vscode ~= 1,
-    opts = {
-        auto_insert_mode = false,
-        language = "Japanese",
-    },
-}
