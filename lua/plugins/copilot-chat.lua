991c76ebf0683be544c99e264f0ce332b424e177
Uehara Masato <masato.uehara.1975@gmail.com>
2026-02-19 21:03:31 +0900

    commit on wsl2

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,11 @@
+return {
+    {
+
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            auto_insert_mode = false,
+            language = "Japanese",
+        },
+    },
+}
